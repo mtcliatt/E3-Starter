bd3d099b944424733e49e2b000e9927bd09b14ca
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-06-19 22:29:34 -0400

    Makes everthing easier to understand and use

--- README.md
+++ README.md
@@ -1,39 +1,19 @@
 # Starter template application for Electron.js and Three.js
 
-### Description
+## Running the application
+Once you have installed electron, run this application by running `electron .`
+from this directory. Click and drag your mouse to rotate the scene.
 
-##### package.json
-The package.json file tells Electron.js to start by running main.js
+## Description of files
 
-##### main.js
-main.js creates an electron window to hold our html and run our JavaScript code
-main.js also tells the window to load the file js/index.html
+ - The package.json file tells Electron that the entry point of the program
+is `main.js`
 
-##### index.html
-index.html includes app.js in script tags and creates any HTML elements to be in the window
+ - main.js create our 'BrowserWindow' and loads `index.html`
+in that window.
 
-##### app.js
-app.js draws the graphics on the screen
-app.js uses the js/three.js, which is the Three.js library, and js/setup.js, which initializes the Three components
-app.js creates and places a grid of cubes in the scene
+ - index.html is just like any other HTML file. It sets the page's title and
+includes our JavaScript files in script tags.
 
-##### three.js
-three.js is the Three.js JavaScript library created by mr. doob
-this file contains everything we need to start working with the Three.js library
-
-##### setup.js
-setup.js intializes the important things we need to work with three.js like the camera, the scene, and the renderer
-setup.js creates a scene, places a camera in that scene, and makes a renderer to render the graphics on the screen
-setup.js reads the variables from js/defaults.js to overwrite the default values used
-
-##### defaults.js
-defaults.js is a file that you can use to change the default values of some properties like:
-
- - Field of view
- - Near and far clipping places
- - Height and width of the area to draw graphics in
- - Window background color
-
-By default, all values in defaults.js are set to undefined, this means that no value is specified and the values will be decided by setup.js. Changing any value from undefined will allow you to specify its value. 
-
+ - app.js is where we initialize our THREE.js components and create our scene.
 
--- js/app.js
+++ js/app.js
@@ -1,33 +1,67 @@
 'use strict';
 
-let THREE = require('./js/three.js');
+let camera;
+let scene;
+let renderer;
+let controls;
 
-// Necessary THREE.js components
-let {camera, scene, renderer} = require('./js/setup');
+init();
+createNeatScene();
+animateNeatScene();
 
-createWorld();
-animate();
+function init() {
+  const fov = 45;
+  const near = 1;
+  const far = 10000;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const backgroundColor = new THREE.Color(0, .2, .3);
+  const opacity = 1.00;
 
-function createWorld() {
+  camera = new THREE.PerspectiveCamera(fov, width / height, near, far);
 
-  // width, height, and depth of each cube and spacing between them
-  const cubeSize    = 1,
-        cubeSpacing = 3;
+  scene = new THREE.Scene;
+  scene.add(camera);
 
-  // Rows, columns, and layers
-  const rows    = 5,
-        columns = 5,
-        layers  = 5;
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  renderer.setClearColor(backgroundColor, opacity);
 
-  // Make BoxGeometry and MeshNormalMaterial once and reuse
-  const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize),
-        material = new THREE.MeshNormalMaterial();
+  controls = new THREE.OrbitControls(camera);
+
+  document.body.appendChild(renderer.domElement);
+
+  window.addEventListener('resize', function() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    renderer.setSize(width, height);
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+  });
+}
+
+function createNeatScene() {
+
+  const cubeSize = 2;
+  const spacing = 5;
+
+  const rows = 15;
+  const columns = 15;
+  const layers = 15;
+
+  const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
 
   // Places a cube object in scene at position specified in parameters
-  let cube;
-  function createCube(x = 0, y = 0, z = 0) {
-    cube = new THREE.Mesh(geometry, material);
-    cube.position.set(x * cubeSpacing, y * cubeSpacing, z * cubeSpacing);
+  function createNeatCube(x = 0, y = 0, z = 0) {
+    const color = new THREE.Color(x / columns, y / rows, z / layers);
+    const material = new THREE.MeshBasicMaterial({
+      color: color
+    });
+
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(x * spacing, y * spacing, z * spacing);
     scene.add(cube);
   }
 
@@ -35,32 +69,25 @@ function createWorld() {
   for (let x = 0; x < columns; x++) {
     for (let y = 0; y < rows; y++) {
       for (let z = 0; z < layers; z++) {
-        createCube(x, y, z);
+        createNeatCube(x, y, z);
       }
     }
   }
 
   // Find the center of the mass of cubes
-  const midX = (columns * cubeSize + (columns - 1) * cubeSpacing) / 2,
-        midY = (rows    * cubeSize + (rows    - 1) * cubeSpacing) / 2,
-        midZ = (layers  * cubeSize + (layers  - 1) * cubeSpacing) / 2;
+  const xMidpoint = (columns * spacing) / 2;
+  const yMidpoint = (rows * spacing) / 2;
+  const zMidpoint = (layers * spacing) / 2;
 
   // Place the camera up and back from the grid for a better view
-  camera.position.set(0, midY * 3, 50);
-
-  // Point the camera at the center of the grid
-  camera.lookAt(new THREE.Vector3(midX, midY, midZ));
+  camera.position.set(-xMidpoint, yMidpoint * 3, zMidpoint * 5);
+  camera.lookAt(new THREE.Vector3(xMidpoint, yMidpoint, zMidpoint));
 }
 
-function animate() {
-  requestAnimationFrame(animate);
+function animateNeatScene() {
+  requestAnimationFrame(animateNeatScene);
+
+  controls.update();
+
   renderer.render(scene, camera);
 }
-
-
-
-
-
-
-
-
